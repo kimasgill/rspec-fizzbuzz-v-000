2aae56fd7f22a7ad4506efc5c3bc7c7808cf39e8
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-10 02:34:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,25 @@
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+# We expect fizzbuzz(3) to return "Fizz"
+# We expect fizzbuzz(5) to return "Buzz"
+# We expect fizzbuzz(15) to return "FizzBuzz"
+# We expect fizzbuzz(4) to return NIL
+
+# Name a method called #fizzbuzz that accepts one argument
+# When I call the method and pass it a number divisible by 3, it should return the string "Fizz"
+# When I call the method and pass it a number divisible by number 5, it should return the string "Buzz"
+# When I call the method and pass it a number divisisible by 3 and 5, it should return the string "FizzBuzz"
+# When I call the method and pass it a number not divisible by 3 or 5, it should return nil.
 
 def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    return "FizzBuzz"
-  elsif num % 5 == 0
-    return "Buzz"
-  elsif num % 3 == 0
-    return "Fizz"
+  if (num % 3 && num % 5)
+    "Fizzbuzz"
+    elsif num % 3 
+    "Fizz"
+    elsif num % 5
+    "Buzz"
+  else
+    nil
   end
 end
-
